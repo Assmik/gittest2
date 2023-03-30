d79cec8172129337859c24c9b014cf9495fcba17
asemkaltaeva <assmee01@gmail.com>
2023-03-30 15:53:16 +0600

    изменение в main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("привет");
+        System.out.println("привет2");
+        System.out.println("привет3");
     }
 }
